9f8ec842c8099e00dad3b67efe33e80db4601e2d
Alex Warren <alexander.ch.warren@gmail.com>
2021-08-02 16:24:20 -0400

    #1: Add FontColor variables to LevelButton

--- Assets/Scripts/LevelButton.cs
+++ Assets/Scripts/LevelButton.cs
@@ -7,12 +7,18 @@ public class LevelButton : MonoBehaviour
     public Button button;
     public Text text;
     public int levelBuildIndex;
+    public Color activeFontColor = Color.black;
+    public Color inactiveFontColor = Color.white;
 
     void Start()
     {
         if (!button.IsInteractable())
         {
-            text.color = Color.white;
+            text.color = inactiveFontColor;
+        }
+        else
+        {
+            text.color = activeFontColor;
         }
     }
 
